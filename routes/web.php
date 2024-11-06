8d3b59bd7fe10e2134f94c87d3dbd621de24ad2e
rizkirmdhnnn <achmadrizkiramadhan0101@gmail.com>
2024-11-06 07:26:12 +0700

    feat: create healthcheck endpoint

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/healthcheck', function () {
+    return response()->json([
+        'status' => true,
+        'message' => 'API is running',
+    ]);
+});
